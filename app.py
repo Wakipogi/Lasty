abe77475851b7519403ac81cf3454d0e6eaeecbb
Wakipogi <90463391+Wakipogi@users.noreply.github.com>
2025-03-27 17:52:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def stop_file():
     return jsonify({"message": f"Stopped {filename}"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
